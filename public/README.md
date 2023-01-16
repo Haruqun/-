38eee772003d62d751dbb513901a36f05a05167f
Haruqun <info@fivenine-design.com>
2023-01-16 17:30:26 +0900

    novel

--- public/README.md
+++
@@ -1,1 +0,0 @@
-# onsen.miniusa.jp
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -11,7 +12,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_that_true_is_true()
+    public function testBasicTest()
     {
         $this->assertTrue(true);
     }
